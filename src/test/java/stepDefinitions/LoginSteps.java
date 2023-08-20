f29a17c7a0570d24c334f46016ec53ae997d60af
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-08-20 16:38:03 +0300

    library login parametrization is added.

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -9,21 +9,55 @@ import utilities.*;
 public class LoginSteps {
 
 
-    LoginPage loginPage =new LoginPage();
+    LoginPage loginPage = new LoginPage();
 
     @Given("User go to library login page")
     public void user_go_to_library_login_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("library1.url"));
     }
+
     @When("User enter student username and password and click login")
     public void user_enter_student_username_and_password_and_click_login() {
         loginPage.userNameInput.sendKeys(ConfigurationReader.getProperty("library.student.email"));
         loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("library.student.password"));
         loginPage.loginBtn.click();
     }
+
+    @When("User enter library username and password and click login")
+    public void user_enter_library_username_and_password_and_click_login() {
+        loginPage.userNameInput.sendKeys(ConfigurationReader.getProperty("library.teacher.email"));
+        loginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("library.teacher.password"));
+        loginPage.loginBtn.click();
+    }
+
     @Then("User should be able to login")
     public void user_should_be_able_to_login() {
         String expected = "Login - Library";
-        Assert.assertEquals(expected,Driver.getDriver().getTitle());
+        Assert.assertEquals(expected, Driver.getDriver().getTitle());
+    }
+
+    @When("user enter {string} information")
+    public void userEnterInformation(String role) {
+
+        String username = "";
+        String password = "";
+        switch (role.toLowerCase()) {
+
+            case "student":
+                username = ConfigurationReader.getProperty("library.student.email");
+                password = ConfigurationReader.getProperty("library.student.password");
+                break;
+
+            case "librarian":
+                username = ConfigurationReader.getProperty("library.teacher.email");
+                password = ConfigurationReader.getProperty("library.teacher.password");
+                break;
+        }
+
+        loginPage.userNameInput.sendKeys(username);
+        loginPage.passwordInput.sendKeys(password);
+        loginPage.loginBtn.click();
+
+
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,8 +1,36 @@
 Feature: Login test with different users
 
 
-  @wip
-  Scenario: Login with student
+  Background:
     Given User go to library login page
+
+  Scenario: Login with student
     When User enter student username and password and click login
     Then User should be able to login
+
+
+  Scenario: login with librarian
+    When User enter library username and password and click login
+    Then User should be able to login
+
+  @wip
+  Scenario Outline: login with parameters
+    When user enter "<role>" information
+    Then User should be able to login
+
+    Examples:
+      | role      | username            | password    |
+      | student   | student5@library    | libraryUser |
+      | librarian | librarian58@library | libraryUser |
+
+
+#  @wip
+#  Scenario Outline: login with parameters
+#    When user enter "<role>" information
+#    And user enter "<username>" and "<password>" for role
+#    Then User should be able to login
+#
+#    Examples:
+#      | role      | username            | password    |
+#      | student   | student5@library    | libraryUser |
+#      | librarian | librarian58@library | libraryUser |
